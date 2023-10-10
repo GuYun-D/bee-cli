7fa438405540fa13ae80d0fec32e8cec8f8d6750
GuYun-D <1946306647@qq.com>
2023-10-10 19:33:59 +0800

    ✨ feat: 添加代码格式化和提交的功能

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 
-const { Command, Option } = require("commander");
-const createCommands = require("./lib/core/create");
-const { initHelpConfig } = require("./lib/core/help");
+const { Command, Option } = require('commander');
+const createCommands = require('./lib/core/create');
+const projectCommands = require('./lib/core/project');
+const { initHelpConfig } = require('./lib/core/help');
 
 const program = new Command();
 
-program.version(require("./package.json").version);
-program.version(require("./package.json").version, "-v", "--version");
+program.version(require('./package.json').version);
+program.version(require('./package.json').version, '-v', '--version');
 
 initHelpConfig(program);
 createCommands(program);
+projectCommands(program);
 
 program.parse();
